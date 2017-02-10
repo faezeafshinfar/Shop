f600b02a1543be9a1be81d7d2a0b85e84a1a466e
faeze <faeze.afshin@gmail.com>
2017-02-10 13:02:18 -0800

    first step

---
+++ Shop/DataLayerShop/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayerShop
+{
+    public class Product
+    {
+        public int ProductCode { get; set; }
+        public int PersianTitle { get; set; }
+        public int EnglishTitle { get; set; }
+        public int AvailableCount { get; set; }
+        public char Producter { get; set; }
+        public char FUllExplain { get; set; }
+        public char ShortExplain { get; set; }
+        public int Price { get; set; }
+        public char Buyer { get; set; }
+        public char ProductDate { get; set; }
+        public char ProductEditDate { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
